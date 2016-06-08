b3e4c789877e1d050b352ac3c60ce4e386cdc6e1
rajat2727 <inforajat27@gmail.com>
2016-06-08 19:42:37 +0530

    Add project files.

---
+++ firstMVC/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using firstMVC.Models;
+using System.Data;
+
+namespace firstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            CodeDB cc = new CodeDB();
+            DataTable objDataTable = cc.getAllMusicTypes();
+            ViewBag.getAllMusicTypes = objDataTable;
+            return View(cc.getAllMusicTypesList());
+
+            //  return Content("This method returns plain texts.");
+
+            //var employee = new
+            //{
+            //    Name = "Rajat Sharma",
+            //    Age = "25",
+            //    Occupation = "Software Developer"
+            //};
+            //return Json(employee, JsonRequestBehavior.AllowGet);
+
+            //return File(Server.MapPath("~/App_Data/documentation.pdf"),
+            //        contentType: "application/pdf",
+            //        fileDownloadName: "documentation.pdf");
+        }
+
+        public ActionResult getMusic()
+        {
+            CodeDB cc = new CodeDB();
+            return View(cc.getAllMusicTypesList());
+        }
+        //public ActionResult Index()
+        //{
+        //    return RedirectToAction("Contact");
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    return Content("This is the redirected Contact method.");
+        //}
+    }
+}
---
+++ firstMVC/Models/CodeDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ApplicationBlocks.Data;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace firstMVC.Models
+{
+    public class CodeDB
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["MusicStoreEntities"].ConnectionString;
+
+        //public CodeDB()
+        //{
+
+        //}
+
+        public DataTable getAllMusicTypes()
+        {
+            string strSql = "select * from Genre";
+            return SqlHelper.ExecuteDataset(connectionString, CommandType.Text, strSql).Tables[0];
+        }
+
+        public IEnumerable<MusicDetails> getAllMusicTypesList()
+        {
+            string strSql = "select * from Genre";
+            return ConvertToTankReadings(SqlHelper.ExecuteDataset(connectionString, CommandType.Text, strSql).Tables[0]);
+        }
+
+        private IEnumerable<MusicDetails> ConvertToTankReadings(DataTable dataTable)
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                yield return new MusicDetails
+                {
+                    id = Convert.ToInt32(row["GenreId"]),
+                    Name = Convert.ToString(row["Name"]),
+                    Description = Convert.ToString(row["Description"])
+                };
+            }
+        }
+    }
+}
